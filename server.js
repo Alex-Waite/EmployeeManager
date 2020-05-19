3c59e576b5c9ddb841a3dc6f56dd2ce0b215846c
Alex Waite <alexanderconorwaite@gmail.com>
2020-05-20 02:21:54 +0930

    inquirer pathing set up

--- server.js
+++ server.js
@@ -12,9 +12,62 @@ const connection = mysql.createConnection({
     password: 'Killer88',
     database: 'employees_db'
 });
+connection.query = util.promisify(connection.query);
+connect = util.promisify(connection.connect);
 
 connection.connect(function (error) {
     if (error) throw error;
     else console.log("connected as id " + connection.threadId)
 });
-connection.query = util.promisify(connection.query);
+
+
+appStarter = async function () {
+    // First must connect to the server before anything else can happen
+    await connect
+    // Loops the app untill this is made "false" (when user exits)
+    while (true) {
+        // Inq prompt to determine what the user wants to do
+        function task() {
+            return inquirer.prompt({
+                name: "whatTask",
+                type: "list",
+                message: "Please pick a task",
+                choices: ["View", "Add", "Update employee roles", "Exit"]
+            })
+        }
+
+        // Determines where to go based on user chosen task
+        let selectedTask = await task()
+
+        if (selectedTask.whatTask === "View") {
+            // Viewing existing data path
+            console.log("View pathing all g")
+
+            function viewWhat() {
+                return inquirer.prompt({
+                    name: "whatToView",
+                    type: "list",
+                    message: "What would you like to view?",
+                    choices: ["Departments", "Roles", "Employees", "Go-Back"]
+                })
+            }
+
+
+        } else if (selectedTask.whatTask === "Add") {
+            // Adding new data path
+            console.log("Add pathing all g")
+
+        } else if (selectedTask.whatTask === "Update employee roles") {
+            // Updating existing data path
+            console.log("Update pathing all g")
+
+        } else if (selectedTask.whatTask === "Exit") {
+            // Exit path
+            console.log("Exit Works")
+            return false
+        }
+
+    }
+}
+
+appStarter()
